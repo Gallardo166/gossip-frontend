62ca18eb9a309735ea663b1272873b127643cbda
Gallardo Lee <leegallardo16@gmail.com>
2025-01-16 02:31:29 +0800

    Create sorting by date or likes options

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react";
 import FilterSystem from "./FilterSystem";
 import Post from "./Post";
 import type { PostPreviewType } from "../../../types/PostPreview";
-import { useOutletContext, useSearchParams } from "react-router";
+import { useNavigate, useOutletContext, useSearchParams } from "react-router";
 import fetchData from "../../../utils/fetchData";
 import { Category } from "../../../types/Category";
+import createURL from "../../../utils/createURL";
 
 const Home = () => {
   const [posts, setPosts] = useState<PostPreviewType[]>([]);
   const {categories} = useOutletContext<{ categories: Category[] }>();
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (searchParams.toString()) {
@@ -22,6 +24,8 @@ const Home = () => {
   return (
     <>
       <FilterSystem categories={categories} />
+      <button onClick={() => navigate(createURL("sort", "time", searchParams))}>By time</button>
+      <button onClick={() => navigate(createURL("sort", "favourite", searchParams))}>By favourite</button>
       <main>
         {posts 
           ? posts.map((post, index) => (
